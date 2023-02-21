06c962cc55dc6d56bf44f9401b138ae579125207
Aryan-Malik-076 <125669058+Aryan-Malik-076@users.noreply.github.com>
2023-02-21 07:08:12 -0800

    Add files via upload

--- 17.cpp
+++ 17.cpp
@@ -1,37 +1,20 @@
-#include<iostream>
-using namespace std;
 
+
+//Find the sum of first and last digit of a numbe//
+
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	
-    int aryan[100], malik[100], haider[100];
-	int r, t, p, x;
-	
-    cout<<"Please Enter the Size of First Array: "<<endl;;
-    cin>>r;
-    cout<<"Enter "<<r<<" Elements for First Array: "<<endl;
-    for(p=0; p<r; p++)
-    {
-    cin>>aryan[p];
-    haider[p] = aryan[p];
-    }
-	x = p;
-    cout<<"\n Please Enter the Size Of Second Array: "<<endl;
-    cin>>t;
-    cout<<"Enter "<<t<<" Elements for Second Array: "<<endl;
-    for(p=0; p<t; p++)
-    {
-	cin>>malik[p];
-    haider[x] = malik[p];
-    x++;
-    }
-    
-    
-    
-    cout<<"\nThe Merged Array Is :"<<endl;
-    for(p=0; p<x; p++)
-    cout<<malik[p]<<" ";
-    cout<<endl;
+   int n,first,last,sum;
+
+    cout << " Input any number: ";
+    cin >> n;
+    first = n;
+	last=n % 10;
+	for(first=n;first>=10;first=first/10);
+    cout<<" The first digit of "<<n<<" is: "<<first<<endl;
+    cout<<" The last digit of "<<n<<" is: "<<last<<endl;
+	cout<<" The sum of first and last digit of "<<n<<" is: "<<first+last<<endl;
 }
-    
--- 18.cpp
+++ 18.cpp
@@ -1,49 +1,38 @@
-#include<iostream>
+ // C++ Program to Find Largest number//
+#include<bits/stdc++.h>
 using namespace std;
-
-
 int main()
 {
-	char x;
-	do{
-	int i,j,temp;
-	int faizan[5];
-	cout<<"Please Enter Five Numbers in an Array : "<<endl;
-	for(int i=0; i<5; i++){
-	cin>>faizan[i];
-	}
-	
-	
-	cout<<"The Array You Entered is : "<<endl;
-	for(int i=0; i<5; i++){
-	cout<<faizan[i]<<" ";
-	}
-	
-	
-
-    
-    for(i=0; i<5; i++){
-    for(j=i+1; j<5; j++){
-	if(faizan[j] < faizan[i])
-	{
-	temp = faizan[i];
-    faizan[i] = faizan[j];
-    faizan[j] = temp;
-     }
-    }
-    }
-
-
-    cout<<"\nElements of Array in Ascending Order:"<<endl;
-    for(i=0; i<5; i++)
+  int a, b, c;
+  cout << "Enter the three numbers a, b & c" <<
+           endl;
+  cin >> a >> b >> c;
+   
+  if(a >= b)
+  {
+    if(a >= c)
     {
-        cout<<faizan[i]<<endl;
+      cout << "The Largest Among Three Numbers is : " <<
+               a << endl;
     }
-    
-    int max;
-     i=0; j=4;
-    max=faizan[j]-faizan[i];
-    cout<<"\nMaximum Difference Is :"<<max<<endl;
-};
+    else
+    {
+      cout << "The Largest Among Three Numbers is : " <<
+               c << endl;
+    }
+  }
+  else
+  {
+    if(b >= c)
+    {
+      cout << "The Largest Among Three Numbers is : " <<
+               b << endl;
+    }
+    else
+    {
+      cout << "The Largest Among Three Numbers is : " <<
+               c << endl;
+    }
+  }
+  return 0;
 }
-    
